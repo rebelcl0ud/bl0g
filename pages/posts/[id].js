0ad5725264fcf2279d0b80b331d2b9873f5cfd31
rebelcl0ud <rebelcl0udmedia@gmail.com>
2022-06-15 15:19:27 -0500

    styles; header, footer, main, post page layout

---
+++ pages/posts/[id].js
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import {getPostsID, getPostData} from '../../lib/posts'
+import Layout from '../../shared/layout'
+
+export default function Post({postData}) {
+    const {id, title, date, contentHTML} = postData
+    return (
+        <Layout>
+            <Head>
+                <title>{title}</title>
+            </Head>
+            <h1>{title}</h1>
+            <small>{date}</small>
+            <br />
+            <div dangerouslySetInnerHTML={{__html: contentHTML}}/>
+            <br />
+            <Link href="/">
+                <a>&larr; go back</a>
+            </Link>
+        </Layout>
+       
+    )
+}
+
+export async function getStaticPaths() {
+    // https://nextjs.org/docs/basic-features/data-fetching#the-paths-key-required
+    const paths = getPostsID()
+
+    console.log({paths})
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({params}) {
+    const postData = await getPostData(params.id)
+
+    return {
+        props: {
+            postData
+        }
+    }
+}
---
+++ shared/layout.js
@@ -0,0 +1,13 @@
+import styles from './layout.module.css'
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Layout ({children}) {
+    return (
+      <>
+        <Header />
+        <div className={styles.container}>{children}</div>
+        <Footer/>
+      </>
+    )
+}
