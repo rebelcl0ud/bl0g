8f3a68655d37e1abef2701cf3a3a7c2af5ed4e96
rebelcl0ud <jovanka.placeres@gmail.com>
2022-06-15 17:31:22 -0500

    update; swapped dummy text header/footer, added bio

---
+++ pages/me.js
@@ -0,0 +1,22 @@
+import Layout from "../shared/layout";
+
+const Me = () => {
+  return (
+    <Layout>
+      <h1>me</h1>
+      <section>
+        <p>I have stacks of scribbles-- drawings, words, songs, books, things to do, things to get, stuff to look into, stuff I've learned, stuff I'm learning, etc. Unfortunately, much of the time I look back through them they end up reading like a collection of hieroglyphics.</p>
+
+        <p>So, now I write as I work on a seperate window in Sublime- those files, although a bit scattered, are readable. Sorta.</p>
+
+        <p>My intention: migrate those Sublime files into blog posts not only for easier access and better readability, but perhaps to better solidify what I may have learned in the process.</p>
+
+        <p>I write for me, but if someone stumbles upon this and gets helped and/or entertained smiles I'd find that pretty rad.</p>
+
+        -jO
+      </section>
+    </Layout>
+  )
+}
+
+export default Me;
