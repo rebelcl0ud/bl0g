be2b13cd6a885c2cab9839894ec7bfcfa0e18b95
rebelcl0ud <jovanka.placeres@gmail.com>
2022-06-16 17:15:10 -0500

    update style/mobile

--- shared/layout.js
+++ shared/layout.js
@@ -1,13 +1,13 @@
-import styles from './layout.module.css'
+import styles from "./layout.module.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-export default function Layout ({children}) {
+export default function Layout({ children }) {
     return (
-      <>
-        <Header />
-        <div className={styles.container}>{children}</div>
-        <Footer/>
-      </>
-    )
+        <>
+            <Header />
+            <div className={styles.container}>{children}</div>
+            <Footer />
+        </>
+    );
 }
