be4a7f4ce2b0df5245bdd3d202b7ff3b657363c8
rebelcl0ud <y0j0v33@gmail.com>
2024-02-13 15:31:07 -0600

    update: header to match footer about

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,18 +2,20 @@ import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
 const Header = () => {
-  return (
-    <header className={styles.header}>
-      <div className={styles.siteInfo}>
-        <Link href="/" className={styles.siteTitle}>COD3nigma</Link>
-        <nav className={styles.siteNav}>
-          <div className={styles.siteNavLinks}>
-            <Link href="/me">me</Link>
-          </div>
-        </nav>
-      </div>
-    </header>
-  )
-}
+    return (
+        <header className={styles.header}>
+            <div className={styles.siteInfo}>
+                <Link href='/' className={styles.siteTitle}>
+                    COD3nigma
+                </Link>
+                <nav className={styles.siteNav}>
+                    <div className={styles.siteNavLinks}>
+                        <Link href='/me'>/me</Link>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    );
+};
 
 export default Header;
